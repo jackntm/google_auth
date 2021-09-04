2d34185af30fe80fe17c0b04f52f6c5f3ee78224
nguyenquochung2357 <carzygaming2222@gmail.com>
2021-09-04 22:19:41 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('public'));
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+app.get('/', function(req,res){
+    res.render('login.ejs');
+})
+app.listen(3000,()=>{
+    console.log('This web is running at http://localhost:'+3000);
+})
